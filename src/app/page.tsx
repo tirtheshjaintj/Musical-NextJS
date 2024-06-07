a49ab2b373506a6447f76bd5f94b69e340464ff9
tj <tirtheshjaintj@gmail.com>
2024-06-07 19:14:45 +0530

    First Next JS project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]" style={{scrollBehavior:"smooth"}}>
 <HeroSection/>
 <FeaturedCourses/>
-<WhyChooseUs/>
+{/* <WhyChooseUs/> */}
 <MusicSchoolTestimonials/>
 <UpcomingWebinars/>
 <Instructors/>
